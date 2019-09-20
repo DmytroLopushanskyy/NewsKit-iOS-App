f70a9905daca65878e1cf3b44ed8d463761d0119
Oleh <tyzhai.o@ucu.edu.ua>
2019-09-20 16:11:41 +0300

    Initial

---
+++ NewsKit-iOS-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsKit-iOS-App
+//
+//  Created by Олег on 20.09.2019.
+//  Copyright © 2019 OlehTyzhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
