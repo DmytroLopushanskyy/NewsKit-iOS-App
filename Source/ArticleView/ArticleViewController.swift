12c2925d8892486269249d4637714a3aced9d9e1
Oleh <tyzhai.o@ucu.edu.ua>
2019-09-24 11:27:39 +0300

    navigation added

---
+++ Source/ArticleView/ArticleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArticleViewController.swift
+//  NewsKit-iOS-App
+//
+//  Created by Олег on 22.09.2019.
+//  Copyright © 2019 OlehTyzhai. All rights reserved.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Source/Extensions/ImageView.swift
@@ -0,0 +1,9 @@
+//
+//  ImageView.swift
+//  NewsKit-iOS-App
+//
+//  Created by Олег on 24.09.2019.
+//  Copyright © 2019 OlehTyzhai. All rights reserved.
+//
+
+import Foundation
